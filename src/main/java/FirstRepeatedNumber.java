40a94c25b5a1743cfcd09a31225d0cedd725c5a2
Abhinav Wadhankar <abhinav.wadhankar@cognizant.com>
2021-07-13 20:40:45 +0530

    code for FirstRepeatedNumber

--- src/main/java/FirstRepeatedNumber.java
+++ src/main/java/FirstRepeatedNumber.java
@@ -1,5 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.TreeSet;
+
 public class FirstRepeatedNumber {
 
+   public void findFirstRepeatedNumber(int[] arr)
+   {
+       int firstRepeatedNumbers=-1;
+       TreeSet treeSet =new TreeSet();
+       for(int i=0;i< arr.length;i++)
+       {
+           if(!treeSet.add(arr[i]))
+           {
+               firstRepeatedNumbers=arr[i];
+               break;
+           }
+       }
+       if(firstRepeatedNumbers!=-1)
+        System.out.println("First Repeated Numbers "+firstRepeatedNumbers);
+       else
+        System.out.println("All Numbers are Unique");
 
+   }
 
 }
--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -1,5 +1,13 @@
 public class MainClass {
 
 
+    public static void main(String args[])
+    {
+        FirstRepeatedNumber firstRepeatedNumber=new FirstRepeatedNumber();
+        int[] arr = new int[]{1, 3, 4, 5, 6, 7, 8};
+        int[] arr1 = new int[]{1, 3, 3, 5, 5, 6, 7, 8};
+        firstRepeatedNumber.findFirstRepeatedNumber(arr);
+        firstRepeatedNumber.findFirstRepeatedNumber(arr1);
+    }
 
 }
